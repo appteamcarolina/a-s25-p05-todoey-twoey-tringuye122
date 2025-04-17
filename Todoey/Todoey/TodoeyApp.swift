d38f481560049d3c740e15c1010992f5fc3a6207
Tri Nguyen <tringuye@unc.edu>
2025-04-17 16:44:06 -0400

    Todoey backend

---
+++ Todoey/Todoey/TodoeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoeyApp.swift
+//  Todoey
+//
+//  Created by Tri Nguyen on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
